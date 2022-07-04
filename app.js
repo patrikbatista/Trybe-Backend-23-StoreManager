e8f7c26611fb72328c8fd5da30d8433ecc905660
patrik <pcr_batista@hotmail.com>
2022-07-04 11:12:19 -0300

    feat: implementa middleware de erro

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+require('express-async-errors');
+
+const handleError = require('./middlewares/handlerError');
 
 const app = express();
 
+app.use(handleError);
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
@@ -9,5 +13,5 @@ app.get('/', (_request, response) => {
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
+// você deve usar o arquivo index.js para executar sua aplicação
 module.exports = app;
