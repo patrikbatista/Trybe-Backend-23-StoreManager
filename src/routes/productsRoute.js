bf0447ab4b577fbf29bf9a4bafef956b651d3af5
patrik <pcr_batista@hotmail.com>
2022-07-04 14:02:44 -0300

    feat: implementa rota que busca produto pelo id

--- src/routes/productsRoute.js
+++ src/routes/productsRoute.js
@@ -3,6 +3,7 @@ const productController = require('../controllers/productController');
 
 const product = Router();
 
+product.get('/:id', productController.getId);
 product.get('/', productController.getAll);
 
 module.exports = product;
