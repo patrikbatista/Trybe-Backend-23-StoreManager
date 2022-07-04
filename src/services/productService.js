357a4fd0fc88cb578c28db9543c8b9000c622270
patrik <pcr_batista@hotmail.com>
2022-07-04 14:06:11 -0300

    :recycle: refactor: refatora logica do getId/service

--- src/services/productService.js
+++ src/services/productService.js
@@ -14,7 +14,7 @@ const getAll = async () => {
 
 const getId = async (id) => {
   const resul = await productModel.getId(id);
-  if (!resul) {
+  if (resul.length === 0) {
     const error = {
       status: 404,
       message: 'Product not found',
