4f234fc7eedc1fdc92739a52e79397b524efa90f
patrik <pcr_batista@hotmail.com>
2022-07-04 16:18:50 -0300

    feat: implementa validacao name do produto

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -13,6 +13,11 @@ const getId = async (req, res) => {
 
 const addProduct = async (req, res) => {
   const { name } = req.body;
+  if (!name) return res.status(400).json({ message: '"name" is required' });
+  if (name.length < 5) {
+    return res.status(422).json({ message: '"name" length must be at least 5 characters long' });
+  }
+
   const newProduct = await productService.addProduct(name);
   res.status(201).json(newProduct);
 };
