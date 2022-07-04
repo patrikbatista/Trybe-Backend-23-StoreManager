0a7aaa20214784c578ab3b6c0cb59dd51996eb71
patrik <pcr_batista@hotmail.com>
2022-07-04 16:37:05 -0300

    feat: implementa funcao na model que insere uma venda

---
+++ src/models/salesModel.js
@@ -0,0 +1,18 @@
+const connection = require('./connection');
+
+const postSales = async (array) => {
+  const [newSale] = await connection
+  .query('INSERT INTO sales_products (sale_id, product_id, quantity) VALUES ?',
+  [array]);
+  return newSale;
+};
+
+const postNewSale = async () => {
+  const [result] = await connection.query('INSERT INTO sales (date) VALUES (now())');
+  return result.insertId;
+};
+
+module.exports = {
+  postSales,
+  postNewSale,
+};
