2b794b95c2cbe62eb41f16868c3a0bfccf8c6959
patrik <pcr_batista@hotmail.com>
2022-07-04 14:01:09 -0300

    feat: implementa funcao getId no controller de products

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -5,6 +5,13 @@ const getAll = async (req, res) => {
   res.status(200).json(products);
 };
 
+const getId = async (req, res) => {
+  const { id } = req.params;
+  const product = await productService.getId(id);
+  res.status(200).json(product);
+};
+
 module.exports = {
   getAll,
+  getId,
 };
