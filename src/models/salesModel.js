de2044e447a75e3d1a965dbdfff65ed69969e22f
patrik <pcr_batista@hotmail.com>
2022-07-05 20:18:35 -0300

    feat: implementa funcoes getSales e getSalesId no model de sales

--- src/models/salesModel.js
+++ src/models/salesModel.js
@@ -12,7 +12,39 @@ const postNewSale = async () => {
   return result.insertId;
 };
 
+const getSales = async () => {
+  const [result] = await connection
+  .query(`SELECT
+  sale_id as saleId,
+  date,
+  product_id,
+  quantity
+  FROM
+  StoreManager.sales_products as sp
+  INNER JOIN StoreManager.sales as venda
+  ON
+  sp.sale_id = venda.id;`);
+  return result;
+};
+
+const getSaleId = async (id) => {
+  const [result] = await connection
+  .query(`SELECT
+  date,
+  product_id,
+  quantity
+  FROM
+  StoreManager.sales_products as sp
+  INNER JOIN StoreManager.sales as venda
+  ON
+  sp.sale_id = venda.id
+  WHERE sp.sale_id = ?;`, [id]);
+  return result;
+};
+
 module.exports = {
   postSales,
   postNewSale,
+  getSales,
+  getSaleId,
 };
