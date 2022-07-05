4963c725d078f307992d2ccbe0a780418f374f2f
patrik <pcr_batista@hotmail.com>
2022-07-05 20:26:38 -0300

    feat: implementa services getSale e getSaleId

--- src/services/saleService.js
+++ src/services/saleService.js
@@ -12,6 +12,32 @@ const addSale = async (arraySales) => {
   return sale;
 };
 
+const getSales = async () => {
+  const result = await saleModel.getSales();
+  if (result.length === 0) {
+    const error = {
+      status: 404,
+      message: 'Sale not found',
+    };
+    throw error;
+  }
+  return result;
+};
+
+const getSaleId = async (id) => {
+  const resul = await saleModel.getSaleId(id);
+  if (!resul) {
+    const error = {
+      status: 404,
+      message: 'Sale not found',
+    };
+    throw error;
+  }
+  return resul;
+};
+
 module.exports = {
   addSale,
+  getSales,
+  getSaleId,
 };
