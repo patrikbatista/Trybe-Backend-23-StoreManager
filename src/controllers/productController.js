d87a8b22ecb8a58ac0cb9a55d395c3ac3097a759
patrik <pcr_batista@hotmail.com>
2022-07-11 14:04:37 -0300

    fix: corrige erros de implementacao

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -3,17 +3,20 @@ const productSchemas = require('../schemas/productSchemas');
 
 const getAll = async (req, res) => {
   const products = await productService.getAll();
+
   return res.status(200).json(products);
 };
 
 const getId = async (req, res) => {
   const { id } = req.params;
+
   const product = await productService.getId(id);
   return res.status(200).json(product);
 };
 
 const addProduct = async (req, res) => {
   const { name } = req.body;
+
   if (!name) return res.status(400).json({ message: '"name" is required' });
   if (name.length < 5) {
     return res.status(422).json({ message: '"name" length must be at least 5 characters long' });
